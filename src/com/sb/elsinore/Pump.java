bd646bb64843afdf28cb83d2249bb880843cd7b1
DougEdey <Doug.edey@gmail.com>
2014-04-06 18:32:20 -0400

    Fix checkstyle in Pump

--- src/com/sb/elsinore/Pump.java
+++ src/com/sb/elsinore/Pump.java
@@ -3,55 +3,85 @@ package com.sb.elsinore;
 import jGPIO.InvalidGPIOException;
 import jGPIO.OutPin;
 
+/**
+ * A helper class for pump control. not very complex.
+ * Designed to control a single GPIO pin with a
+ * straight forward on/off functionality
+ * @author Doug Edey
+ *
+ */
 public class Pump {
-    
-    /*
-     * Pump class is designed to control a single GPIO pin with a straight forward on/off functionality 
+
+    /**
+     * Current pump name.
      */
-    
-    public String name;
-    public OutPin output = null;
-    
-    public Pump(String name, String pinName) throws InvalidGPIOException {
-        this.name = name.replace("_", " ");
+    private String name;
+    /**
+     * the outpin for the pump.
+     */
+    private OutPin output = null;
+
+    /**
+     * The Constructor.
+     * @param newName The name of this pump
+     * @param pinName The GPIO pin name
+     * @throws InvalidGPIOException If there's a problem opening the GPIO.
+     */
+    public Pump(final String newName, final String pinName)
+            throws InvalidGPIOException {
+        this.name = newName.replace("_", " ");
         try {
-            output = new OutPin(pinName);
+            this.output = new OutPin(pinName);
         } catch (InvalidGPIOException e) {
-            // TODO Auto-generated catch block
             throw e;
         } catch (RuntimeException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
-    
-    public boolean getStatus() {
+
+    /**
+     * @return The current state of the pump, true for on. False for off.
+     */
+    public final boolean getStatus() {
         try {
             return output.getValue().equals("1");
         } catch (Exception e) {
             return false;
         }
     }
-    
-    public void turnOn() {
+
+    /**
+     * Turn on the pump.
+     */
+    public final void turnOn() {
         output.setValue(true);
     }
-    
-    public void turnOff() {
+
+    /**
+     * Turn off the pump.
+     */
+    public final void turnOff() {
         output.setValue(false);
     }
 
-    public String getName() {
-        // TODO Auto-generated method stub
+    /**
+     * @return The name of this pump
+     */
+    public final String getName() {
         return name;
     }
 
-    public String getGPIO() {
-        // TODO Auto-generated method stub
+    /**
+     * @return The current GPIO Pin.
+     */
+    public final String getGPIO() {
         return output.getGPIOName();
     }
 
-    public String getNodeName() {
+    /**
+     * @return Return the current name with _ instead of spaces.
+     */
+    public final String getNodeName() {
         return name.replace(" ", "_");
     }
 }
