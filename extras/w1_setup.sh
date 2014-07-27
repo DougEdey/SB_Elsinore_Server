af320a10f710148c5e1394306e1c498b1a4c2b84
DougEdey <Doug.edey@gmail.com>
2014-07-27 12:24:47 -0400

    Update setup script for GPIO and W1 on BBB

--- extras/w1_setup.sh
+++ extras/w1_setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Setting up w1"
-echo w1 > /sys/devices/bone_capemgr.8/slots
+echo w1 > /sys/devices/bone_capemgr.*/slots
+echo "Setting up jgpio"
+echo jgpio > /sys/devices/bone_capemgr.*/slots
 echo "Cape Manager updated, list of devices:"
 ls -ltr /sys/devices/w1_bus_master1
