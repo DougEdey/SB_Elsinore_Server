9722ddb4d7b3528bc40487c709780ce021ffbfe8
DougEdey <Doug.edey@gmail.com>
2015-01-23 19:14:56 -0500

    Fix stop time of timer

--- src/com/sb/elsinore/BrewDay.java
+++ src/com/sb/elsinore/BrewDay.java
@@ -175,7 +175,7 @@ public final class BrewDay {
             continueTimer(name);
         } else {
             Entry<String, Date> stopEntry = new AbstractMap.SimpleEntry<String, Date>(
-                    "end", parseDateString(stopIn));
+                    "end", new Date());
             setTimer(name, stopEntry);
         }
     }
