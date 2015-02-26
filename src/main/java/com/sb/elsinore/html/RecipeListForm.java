a2121c8120267935ff7458dcb2763b88a16e091a
DougEdey <Doug.edey@gmail.com>
2015-02-25 21:03:42 -0500

    Improve selection of the Recipe

--- src/main/java/com/sb/elsinore/html/RecipeListForm.java
+++ src/main/java/com/sb/elsinore/html/RecipeListForm.java
@@ -6,31 +6,31 @@ import org.rendersnake.Renderable;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Map;
 
 import static org.rendersnake.HtmlAttributesFactory.id;
 import static org.rendersnake.HtmlAttributesFactory.name;
 import static org.rendersnake.HtmlAttributesFactory.value;
 
 /**
- * Created by doug on 07/02/15.
+ * Render a form allowing selection of the recipe.
+ * Created by Doug Edey on 07/02/15.
  */
 public class RecipeListForm implements Renderable {
 
     @Override
     public void renderOn(HtmlCanvas htmlCanvas) throws IOException {
         ArrayList<String> recipeList = BrewServer.getRecipeList();
-        htmlCanvas.div(id("newTriggersForm"))
+        htmlCanvas.div(id("selectRecipeForm"))
             .form()
-                .select(name("type").class_("holo-spinner")
+                .select(name("name").class_("holo-spinner")
                 .onClick("setRecipe(this);"));
             htmlCanvas.option(value("").selected_if(true))
-                    .write("Select Recipe Type")
+                    .write("Select Recipe")
                     ._option();
             for (String entry: recipeList) {
                 htmlCanvas.option(value(entry))
-                        .write(entry)
-                        ._option();
+                    .write(entry)
+                    ._option();
             }
             htmlCanvas._select()
         ._form()
