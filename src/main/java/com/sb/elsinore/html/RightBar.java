ac8aeef8e3bd1e3e198a5289bc9c6ca131b546c2
DougEdey <Doug.edey@gmail.com>
2015-02-26 10:57:42 -0500

    Add spans for the buttons to show the recipe details.

--- src/main/java/com/sb/elsinore/html/RightBar.java
+++ src/main/java/com/sb/elsinore/html/RightBar.java
@@ -3,6 +3,7 @@ package com.sb.elsinore.html;
 import java.io.IOException;
 
 import com.sb.elsinore.BrewServer;
+import com.sb.elsinore.Messages;
 import org.rendersnake.HtmlCanvas;
 import org.rendersnake.Renderable;
 
@@ -16,19 +17,19 @@ public class RightBar implements Renderable {
             .div(class_("breweryImage"))
                 .img(class_("center-block").height("200").width("200").id("brewerylogo").src(" "))
             ._div()
-            .span(class_("btn btn-default btn-file")).write("Upload Logo")
+            .span(class_("btn btn-default btn-file")).write(Messages.UPLOAD_LOGO)
                 .input(type("file").id("logo").add("data-url", "uploadImage"))
             ._span()
 
             .div(class_("recipe"))
             ._div()
-            .span(class_("btn btn-default btn-file")).write("Upload BeerXML")
+            .span(class_("btn btn-default btn-file")).write(Messages.UPLOAD_BEERXML)
                 .input(type("file").id("beerxml").add("data-url", "uploadbeerxml"))
             ._span();
 
         if (BrewServer.getRecipeList() != null && BrewServer.getRecipeList().size() > 1) {
-            html.select(name("selectRecipe").class_("holo-spinner")
-                .onSelect("chooseRecipe(this);"));
+            html.select(id("selectRecipe").class_("holo-spinner")
+                .onSelect("showRecipe(this);"));
             html.option(value("").selected_if(true))
                     .write("Select Recipe")
                     ._option();
@@ -38,6 +39,12 @@ public class RightBar implements Renderable {
                         ._option();
             }
             html._select();
+        }
+
+        if (BrewServer.getCurrentRecipe() != null) {
+            html.span(id("showCurrentRecipe").class_("btn btn-default")
+                    .onClick("showRecipe(this)")).write(Messages.SHOW_RECIPE)
+            ._span();
         }
         html._div();
 
