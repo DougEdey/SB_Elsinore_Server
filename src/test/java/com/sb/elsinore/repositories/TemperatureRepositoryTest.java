d7173e3dd9373d3e065ad17ffaf40f2d0869dc1c
Doug Edey <doug.edey+github@gmail.com>
2018-08-29 21:34:49 -0400

    More TemperatureRepo Tests

--- src/test/java/com/sb/elsinore/repositories/TemperatureRepositoryTest.java
+++ src/test/java/com/sb/elsinore/repositories/TemperatureRepositoryTest.java
@@ -1,23 +1,22 @@
 package com.sb.elsinore.repositories;
 
-import com.sb.elsinore.LaunchControl;
-import com.sb.elsinore.TemperatureListeners;
 import com.sb.elsinore.configuration.JpaDataConfiguration;
 import com.sb.elsinore.configuration.TestJpaConfiguration;
 import com.sb.elsinore.configuration.TestRestConfiguration;
 import com.sb.elsinore.models.Temperature;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.junit.rules.ExpectedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 @SpringBootTest
@@ -27,15 +26,13 @@ import static org.mockito.MockitoAnnotations.initMocks;
         TestRestConfiguration.class})
 @DataJpaTest
 public class TemperatureRepositoryTest extends AbstractTransactionalJUnit4SpringContextTests {
-    @Mock
-    LaunchControl launchControl;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
     @Autowired
     private TestEntityManager entityManager;
     @Autowired
     private TemperatureRepository temperatureRepository;
-
-    @InjectMocks
-    private TemperatureListeners temperatureListeners;
 
     @Before
     public void setup() {
@@ -57,6 +54,81 @@ public class TemperatureRepositoryTest extends AbstractTransactionalJUnit4Spring
 
         //then
         assertEquals(temperature.getName(), found.getName());
+    }
+
+    @Test
+    public void canDeleteTemperature() {
+        // Given
+        Temperature temperature = new Temperature();
+        temperature.setName("Test");
+        temperature.setDevice("Device");
+
+        this.entityManager.persist(temperature);
+        this.entityManager.flush();
+
+        // when
+        this.temperatureRepository.delete(temperature);
+
+        // then
+        assertNull(this.temperatureRepository.findByName(temperature.getName()));
+    }
+
+    @Test
+    public void canSaveTemperatureViaRepository() {
+        // Given
+        Temperature temperature = new Temperature();
+        temperature.setName("Test");
+        temperature.setDevice("Device");
+
+        // when
+        this.temperatureRepository.save(temperature);
+
+        // then
+        assertEquals(temperature, this.temperatureRepository.findByName(temperature.getName()));
+    }
+
+    @Test
+    public void canUpdateTemperatureName() {
+        // Given
+        Temperature temperature = new Temperature();
+        temperature.setName("Test");
+        temperature.setDevice("Device");
+
+        // when
+        this.temperatureRepository.save(temperature);
+
+        // then
+        assertNotNull(this.temperatureRepository.findByName("Test"));
+        assertNull(this.temperatureRepository.findByName("Test Updated"));
+
+        // given
+        temperature.setName("Test Updated");
+
+        // when
+        this.temperatureRepository.save(temperature);
+
+        // then
+        assertNull(this.temperatureRepository.findByName("Test"));
+        assertNotNull(this.temperatureRepository.findByName("Test Updated"));
+    }
+
+    @Test
+    public void temperatureNameMustBeUnique() {
+        // Given
+        Temperature temperature = new Temperature();
+        temperature.setName("Test");
+        temperature.setDevice("Device");
+
+        Temperature copyTemperature = new Temperature(temperature);
+
+        // when
+        this.temperatureRepository.saveAndFlush(temperature);
+
+        // then
+        this.thrown.expect(DataIntegrityViolationException.class);
+        this.temperatureRepository.saveAndFlush(copyTemperature);
+
+
     }
 
 }
